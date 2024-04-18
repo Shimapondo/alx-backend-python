d3321f98d3aeaed7526e481f52c742938a4c343c
Shimapondo <williamnamabende@gmail.com>
2024-04-18 22:11:38 +0000

    defining my variables

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''module for task 3
+'''
+
+
+def to_str(n: float) -> str:
+    ''' cast a floating type number in to a string.
+    '''
+    return str(n)
---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env pyhton3
+'''task 4 module
+'''
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: boo1 = True
+school: str = 'Holberton'
